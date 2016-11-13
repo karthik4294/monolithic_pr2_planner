cee375a557f0671281a5bff2cfeb4aa49d79fe14
Karthik <karthik4294@gmail.com>
2016-11-13 13:19:48 -0500

    add learner node

---
+++ src/monolithic_pr2_planner_node/include/monolithic_pr2_planner_node/learn_policy.h
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <monolithic_pr2_planner/Environment.h>
+#include <monolithic_pr2_planner_node/EnvInterfaces.h>
+#include <boost/shared_ptr.hpp>
+#include <memory>
+
+namespace monolithic_pr2_planner_node {
+    class LearnPolicy {
+        public:
+            LearnPolicy(ros::NodeHandle nh);
+        private:
+            boost::shared_ptr<monolithic_pr2_planner::Environment> m_env;
+            EnvInterfaces m_env_interface;
+    };
+}
